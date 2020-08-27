9d354a1512a225863cffd7673d5eeaff846e10f5
selam lemma <oneiconforlife26@gmail.com>
2020-08-27 21:21:33 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,43 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
-end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarf_names_array)# code an argument here
+  i = 1
+  dwarf_names_array.each do |dwarf|
+    puts  "#{i}. #{dwarf}"
+    i +=1
+  end
 end
+   # Your code here
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+def summon_captain_planet(planeteer_calls)
+    summon_captain_planet.collect{ |planeteer_calls|
+    planeteer_calls.capitalize} +  "!"
+
+ end
+
+
+ def long_planeteer_calls(array)
+   if array.size > 4
+     false
+   else
+     true
+   end
+array
+ end
+ #   i = 4
+ # while i >  planeteer_calls.length
+ #     if planeteer_calls.length |i| i < 4
+ #    false
+ #   end
+ # end
+ # end
+
+
+
+def find_the_cheese(food)
+  i = 1
+  food.each do |food|
+    puts  "#{i}. #{food}"
+    i += 1
+  end
 end
